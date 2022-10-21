41c5c1dd2c7d39346940cd0a1cb789f9a73f3c9b
Chris Whitworth <chris.whitworth@microsoft.com>
2022-10-21 14:50:18 +0100

    Add logging

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -14,9 +14,21 @@ dotnet pack -o $Feed $(Join-Path $CSharp "Nuget" "Package")
 $nupkgName = Get-ChildItem -Path $Feed -Name -Filter "*.nupkg"
 
 $TestProject = Join-Path $CSharp Nuget Tests DeviceAPITest DeviceAPITest DeviceAPITest.csproj
+
+Write-Host "::debug::Removing package from test project"
+
 dotnet remove $TestProject package Microsoft.Azure.Sphere.DeviceAPI
+
+Write-Host "::debug::Adding locally built package to test project"
+
 dotnet add $TestProject package Microsoft.Azure.Sphere.DeviceAPI -s $Feed
+
+Write-Host "::debug::Restoring packages for test project"
+
 dotnet restore $TestProject -s https://pkgs.dev.azure.com/msazuresphere/4x4/_packaging/4x4_PublicPackages/nuget/v3/index.json
+
+Write-Host "::debug::Building test project"
+
 dotnet build $TestProject
 
 Remove-Item -Recurse $WorkingFolder
