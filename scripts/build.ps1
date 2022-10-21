6e43e0fd31c6fa7d22d3316e104d411124049fff
Chris Whitworth <chris.whitworth@microsoft.com>
2022-10-21 14:43:27 +0100

    Explicitly configure package feed

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -16,6 +16,7 @@ $nupkgName = Get-ChildItem -Path $Feed -Name -Filter "*.nupkg"
 $TestProject = Join-Path $CSharp Nuget Tests DeviceAPITest DeviceAPITest DeviceAPITest.csproj
 dotnet remove $TestProject package Microsoft.Azure.Sphere.DeviceAPI
 dotnet add $TestProject package Microsoft.Azure.Sphere.DeviceAPI -s $Feed
+dotnet restore $TestProject -s https://pkgs.dev.azure.com/msazuresphere/4x4/_packaging/4x4_PublicPackages/nuget/v3/index.json
 dotnet build $TestProject
 
 Remove-Item -Recurse $WorkingFolder
