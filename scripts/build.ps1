1dccebfb1873f53ef2c3497dd187aacb9845ba54
Chris Whitworth <chris.whitworth@microsoft.com>
2022-11-01 11:48:03 +0000

    Build script refactors

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,36 +1,40 @@
+
+function Build-Package($root, $outputFolder)
+{
+    Write-Host "Working in $root"
+    $package = Join-Path $root "Nuget" "Package"
+    Write-Host "Building $package"
+    &dotnet build $package
+    &dotnet pack -o $outputFolder -p:PackageVersion="9999.0.0.0" $package
+}
+
+function Build-Tests($root, $feed)
+{
+    # $nupkgName = Get-ChildItem -Path $feed -Name -Filter "*.nupkg"
+
+    $TestProject = Join-Path $root Nuget Tests DeviceAPITest DeviceAPITest DeviceAPITest.csproj
+
+    & dotnet nuget add source $feed
+
+    & dotnet remove $TestProject package Microsoft.Azure.Sphere.DeviceAPI
+
+    & dotnet add $TestProject package Microsoft.Azure.Sphere.DeviceAPI --version "9999.0.0.0"
+
+    & dotnet restore $TestProject
+
+    & dotnet build $TestProject
+}
+
 $TempFolder = [System.IO.Path]::GetTempPath()
 $WorkingFolder = Join-Path $TempFolder $([string] [System.Guid]::NewGuid())
 New-Item -ItemType Directory -Path $WorkingFolder
+$Feed = Join-Path $WorkingFolder "feed"
 
 $PathToManufacturing = Join-Path -Resolve $PSScriptRoot ".." "Manufacturing"
 
 $CSharp = Join-Path -Resolve $PathToManufacturing "src" "CSharp"
 
-$Feed = Join-Path $WorkingFolder "feed"
-
-dotnet build $(Join-Path $CSharp "Nuget" "Package")
-dotnet pack -o $Feed -p:PackageVersion="9999.0.0.0" $(Join-Path $CSharp "Nuget" "Package") 
-
-$nupkgName = Get-ChildItem -Path $Feed -Name -Filter "*.nupkg"
-
-$TestProject = Join-Path $CSharp Nuget Tests DeviceAPITest DeviceAPITest DeviceAPITest.csproj
-
-dotnet nuget add source $Feed
-
-Write-Host "::debug::Removing package from test project"
-
-dotnet remove $TestProject package Microsoft.Azure.Sphere.DeviceAPI
-
-Write-Host "::debug::Adding locally built package to test project"
-
-dotnet add $TestProject package Microsoft.Azure.Sphere.DeviceAPI --version "9999.0.0.0"
-
-Write-Host "::debug::Restoring packages for test project"
-
-dotnet restore $TestProject
-
-Write-Host "::debug::Building test project"
-
-dotnet build $TestProject
+Build-Package $CSharp $Feed
+Build-Tests $CSharp $Feed
 
 Remove-Item -Recurse $WorkingFolder
